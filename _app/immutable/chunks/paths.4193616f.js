fb136b5e44a170719aa47f3f66cbf618dd865516
haruyuki7121994 <haruyuki7121994@gmail.com>
2023-11-18 21:34:45 +0700

    Updates

---
+++ _app/immutable/chunks/paths.4193616f.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1qryf4j)==null?void 0:s.base)??"/portfolio-ad";var a;const e=((a=globalThis.__sveltekit_1qryf4j)==null?void 0:a.assets)??t;export{e as a,t as b};
